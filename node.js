2c7840ea43cbde74b5d54fa316c9053ecb4ebea3
baldx <mr.ahmad.m2006@gmail.com>
2023-09-19 09:42:44 +0200

    add pretty print

--- node.js
+++ node.js
@@ -65,4 +65,16 @@ function removeDupes (arr) {
     })
 }
 
-console.log(removeDupes(sortArray(array)));
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+  
